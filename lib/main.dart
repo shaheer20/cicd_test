cabc6512c37e94115fec6e434fe7b189b59481c3
macbookpro <shaheer20>
2023-02-27 22:46:04 +0500

    Ziinsa

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
 
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
